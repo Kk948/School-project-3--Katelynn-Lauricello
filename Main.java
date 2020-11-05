73c10a4d56b03ef110e3ceb87a57478c786045b4
Katelynn Lauricello <katelynn.lauricello@patelhs.org>
2020-11-05 14:00:31 +0000

    I added for loop

--- Main.java
+++ Main.java
@@ -2,12 +2,14 @@ import java.util.Scanner;
 class Main {
   public static void main(String[] args) {
     int [] seats = {0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15};
-    String [] students = {"Tsukuyomi","Osiris","Aphrodite","Ameratsu","Anubis","Kali","Merlin","Arachne","Bastet","Bellona","Chang'e", "Da ji","Hel","Jing wei","Nike"};
 
      Scanner Scanner= new Scanner(System.in);
-     
+   for(int i=0; i<14; i++)
+    {
       System.out.println("What's your name?");
       String name = Scanner.next();
+      System.out.println(name + "you will sit at " + name [i]);
+    }
 
   }
 }
