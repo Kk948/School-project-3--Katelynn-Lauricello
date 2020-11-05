8b14bd2d10a17501cff94c076c7af8225a586e72
Katelynn Lauricello <katelynn.lauricello@patelhs.org>
2020-11-05 13:54:14 +0000

    I added the seats and students + the scanner

--- Main.java
+++ Main.java
@@ -1,5 +1,13 @@
+import java.util.Scanner;
 class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    int [] seats = {0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15};
+    String [] students = {"Tsukuyomi","Osiris","Aphrodite","Ameratsu","Anubis","Kali","Merlin","Arachne","Bastet","Bellona","Chang'e", "Da ji","Hel","Jing wei","Nike"};
+
+     Scanner Scanner= new Scanner(System.in);
+     
+      System.out.println("What's your name?");
+      String name = Scanner.next();
+
   }
 }
