732fd42ac93b2182b853b7019ee99217e5ca6ae9
Katelynn Lauricello <katelynn.lauricello@patelhs.org>
2020-11-06 18:37:28 +0000

    Done Horray

--- Main.java
+++ Main.java
@@ -1,15 +1,25 @@
 import java.util.Scanner;
 class Main {
   public static void main(String[] args) {
-    int [] seats = {0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15};
+    int [] seats = {0,1,2,3,4,5,6,7,8,9,10,11,12,13,14};
 
-     Scanner Scanner= new Scanner(System.in);
-   for(int i=0; i<14; i++)
+    String [] names = new String [15];
+
+
+     Scanner Scan= new Scanner(System.in);
+   for(int i=0; i<=14; i++)
     {
       System.out.println("What's your name?");
-      String name = Scanner.next();
-      System.out.println(name + "you will sit at " + name [i]);
+      names[i] = Scan.next();
+      System.out.println(names[i] + " you will sit at seat " + seats[i]);
     }
-
+    int i = 0;
+   while (i <= 14)
+   {
+      System.out.println(names[i] + " will sit at" + seats[i]);
+    i++;
+    
+   }
+      
   }
 }
